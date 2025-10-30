7dbf7f16c2300ad20ace727fe00f5edc58e1a395
snowykte0426 <snowykte0426@naver.com>
2025-10-30 19:59:17 +0900

    Add Spring Boot and utility dependencies; create multiple bloat classes for testing

---
+++ src/main/java/com/nextach/megamethod/bloat/BloatClass20.java
@@ -0,0 +1,47 @@
+package com.nextach.megamethod.bloat;
+
+import java.util.*;
+
+public class BloatClass20 {
+    
+    public static String process(String input) {
+        if (input == null) return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            sb.append(input).append(" ");
+        }
+        return sb.toString();
+    }
+    
+    public static int calculate(int a, int b) {
+        int result = 0;
+        for (int i = 0; i < 1000; i++) {
+            result += (a * b) % (i + 1);
+        }
+        return result;
+    }
+    
+    public static List<String> generateData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 500; i++) {
+            data.add("Data item " + i);
+        }
+        return data;
+    }
+    
+    public static Map<String, Integer> generateMap() {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < 300; i++) {
+            map.put("key" + i, i * 100);
+        }
+        return map;
+    }
+    
+    public static void performComputation() {
+        double result = 0;
+        for (int i = 1; i < 10000; i++) {
+            result += Math.sqrt(i) * Math.log(i);
+        }
+        System.out.println("Computation result: " + result);
+    }
+}
